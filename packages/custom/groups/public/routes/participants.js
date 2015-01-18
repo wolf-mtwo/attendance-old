ce51383cdf4fd280c9fdd8c9a0b9c3489b855751
Rolf Mtwo <wolf.mtwo@outlook.com>
2015-01-18 01:19:49 +0100

    Updated btn color and changed files position

--- packages/custom/groups/public/routes/participants.js
+++ packages/custom/groups/public/routes/participants.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('mean.groups').config(['$stateProvider',
-function($stateProvider) {
+  function($stateProvider) {
 
-  var path = 'groups/views/participant/';
-  $stateProvider.state('participant', {
-    url: '/groups/:groupId/participants/:participantId',
-    controller: 'ParticipantsController',
-    templateUrl: path + 'index.html'
-  });
+    var path = 'groups/views/participant/';
+    $stateProvider.state('participant', {
+      url: '/groups/:groupId/participants/:participantId',
+      controller: 'ParticipantsController',
+      templateUrl: path + 'index.html'
+    });
 
-}
+  }
 ]);
