2dbbb9332f17e0c6709ef53dedf08a4fc38a7539
Rolf Mtwo <wolf.mtwo@outlook.com>
2015-02-22 05:42:55 +0100

    Added no-cache option to json request

--- server.js
+++ server.js
@@ -7,4 +7,13 @@ var mean = require('meanio');
 mean.serve({ /*options placeholder*/ }, function(app, config) {
 	var port = config.https && config.https.port ? config.https.port : config.http.port;
 	console.log('Mean app started on port ' + port + ' (' + process.env.NODE_ENV + ')');
+
+	app.use(function(req, res, next) {
+		if(!req.url.split('.')[1]) {
+			res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+			res.header('Pragma', 'no-cache');
+		}
+		next();
+	});
+
 });
